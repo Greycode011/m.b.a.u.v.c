4ac9cfd78ccf7b83049fc6f2f3ee390b88f34e79
moh alqasab <mmalqsab@gmail.com>
2022-12-31 18:12:02 +0300

    0.2.1 db fix

--- screens/Login.js
+++ screens/Login.js
@@ -3,32 +3,37 @@ import { StyleSheet, Text, View ,TextInput,TouchableOpacity,Image} from 'react-n
 import { db,authentication} from '../components/config';
 import { collection, doc, setDoc,addDoc,getDoc,docData,getDocs } from "firebase/firestore"; 
 import React, { useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 function Loginscreen({navigation}){
   const[username,setusername]=useState('');
   const[password,setpassword]=useState('');
+  const[email,setEmail]=useState('');
+  
+    function signIn() {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          //your code
+          navigation.navigate('Home');
+          alert("Sucessfully signed in!");
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorMessage);
+        });
+    }
 
-  function Homespage(){
-    getDoc(doc(db, "users", '5pfPewORCotrSwTDpHN3')).then(docData => { 
-      // Data saved successfully!
-      
-      if (docData.exists()) {
-
-        // console.log(docData.data());
-       
-        setusername(docData.data().username);
-        setpassword(docData.data().password);   
-        navigation.navigate('Home');
-      } else {
-         console.log('No such a data!');
-      }
-
-    }).catch((error) => {
-          // The write failed...
-          console.log(error);
-    })
+  
      
     
-}
+
 
 
 return(
@@ -36,11 +41,11 @@ return(
 
 <StatusBar style="auto" />
 <Image style={styles.image} source={require('../assets/M.B.A.U.V.C.png')}/>
-<TextInput value={username} onChangeText={(username) => {setusername(username)}} style={styles.TextInput} placeholder='Username' placeholderTextColor="#442C2E"  />
+<TextInput value={email} onChangeText={(email) => {setEmail(email)}} style={styles.TextInput} placeholder='Email' placeholderTextColor="#442C2E"  />
 <TextInput value={password}  onChangeText={(password) => {setpassword(password)}} style={styles.TextInput} placeholder='Password' placeholderTextColor="#442C2E" secureTextEntry={true} />
 
 
-<TouchableOpacity style={styles.buttonStyle} onPress={Homespage}><Text >  Login in </Text></TouchableOpacity>
+<TouchableOpacity style={styles.buttonStyle} onPress={signIn}><Text >  Login in </Text></TouchableOpacity>
 <TouchableOpacity>
   <Text style={styles.forgot_button} >Forgot Password?</Text>
 </TouchableOpacity>
--- screens/Register.js
+++ screens/Register.js
@@ -3,36 +3,56 @@ import { StyleSheet, Text, View,Button,TextInput,Image,TouchableOpacity,ScrollVi
 import React, { useState } from 'react';
 import DatePicker from 'react-native-modern-datepicker';
 import { collection, doc, setDoc,addDoc,getDoc } from "firebase/firestore"; 
-import { db } from '../components/config.jsx';
+import { db,authentication  } from '../components/config.jsx';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
-function Registercreen(){
+
+
+function Registercreen({navigation}){
   const [Date, setSelectedDate] = useState('');
   const[email,setEmail]=useState('');
   const[username,setusername]=useState('');
   const[phone,setphone]=useState('');
   const[password,setpassword]=useState('');
   const[fullname,setfullname]=useState('');
-  function create(){
-    //submit data
-    addDoc(collection(db, "users"), {
-      username:username,
-      fullname:fullname,
-      Email: email,
-      phone:phone,
-      password:password,
-      Date:Date,
-
-    }).then(() => { 
-      // Data saved successfully!
-      console.log('data submitted');  
-
-    }).catch((error) => {
-          // The write failed...
-          console.log(error);
-    });
-
+ 
+  function signUp() {
+    createUserWithEmailAndPassword(authentication, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // your code
+        navigation.navigate('Login');
+        alert("Successfully sign up!");
+        addDoc(collection(db, "users"), {
+          username:username,
+          fullname:fullname,
+          email: email,
+          phone:phone,
+          password:password,
+          Date:Date,
+    
+        }).then(() => { 
+          // Data saved successfully!
+          console.log('data submitted');  
+    
+        }).catch((error) => {
+              // The write failed...
+              console.log(error);
+        });
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // ..
+      });
   }
-
 return(
   <ScrollView>
 <View style={styles.container}>
@@ -55,7 +75,7 @@ return(
       }}
       mode="calendar"
       onSelectedChange={date => setSelectedDate(date)}/>
-      <TouchableOpacity style={styles.buttonStyle}   onPress={create}><Text>  Register</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.buttonStyle}   onPress={signUp}><Text>  Register</Text></TouchableOpacity>
      
  
 </View>
